ffdb3f709b51192b36f9ae18a83a3dee4e57596b
Michael Knoch <info@michaelknoch.de>
2015-04-06 15:21:10 +0200

    implement scaffolding

--- app/index.js
+++ app/index.js
@@ -4,56 +4,58 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.pkg = require('../package.json');
-  },
-
-  prompting: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the excellent ' + chalk.red('HapiTypescriptPluginGenerator') + ' generator!'
-    ));
-
-      var prompts = [{
-          name: 'pluginName',
-          message: 'What would be the best name for your Plugin? mh? Choose wisely!'
-      }];
-
-      this.prompt(prompts, function(props) {
-          this.pluginName = props.pluginName;
-
-          done();
-      }.bind(this));
-
-  },
-
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
+    initializing: function () {
+        this.pkg = require('../package.json');
     },
 
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
-  },
+    prompting: function () {
+        var done = this.async();
 
-  install: function () {
-    this.installDependencies();
-  }
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the excellent ' + chalk.red('HapiTypescriptPluginGenerator') + ' generator!'
+        ));
+
+        var prompts = [{
+            name: 'pluginName',
+            message: 'What would be the best name for your Plugin? mh? Choose wisely!'
+        }];
+
+        this.prompt(prompts, function (props) {
+            this.pluginName = props.pluginName;
+
+            done();
+        }.bind(this));
+
+    },
+
+    scaffoldFolders: function () {
+        this.mkdir('src');
+        this.mkdir('test');
+    },
+
+    writing: {
+
+        templates: function () {
+            var context = {
+                site_name: this.appName
+            };
+            this.template('src/_main.ts', 'src/main.ts', context);
+            this.template('src/_plugin.ts', 'src/plugin.ts', context);
+        },
+
+        files: function () {
+            this.copy('gulpfile.js', 'gulpfile.js');
+            this.copy('gitignore', '.gitignore');
+            this.copy('index.js', 'index.js');
+            this.copy('README.md', 'README.md');
+            this.copy('Makefile', 'Makefile');
+            this.copy('test/test.js', 'test/test.js');
+        }
+
+    },
+
+    install: function () {
+        this.installDependencies();
+    }
 });
